031379047f31ee8fca203a34cb991eae36653db5
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-31 07:46:32 +0000

    fix issue with dagger connect
    fix issue with dagger connect

--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -1,5 +1,4 @@
-import { Client, Directory } from "../../sdk/client.gen.ts";
-import { connect } from "../../sdk/connect.ts";
+import { Directory, dag } from "../../sdk/client.gen.ts";
 import { getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -18,27 +17,22 @@ export const exclude = [".git", ".fluentci", "target"];
 export async function uberjar(
   src: Directory | string | undefined = "."
 ): Promise<Directory | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.uberjar)
-      .container()
-      .from("ghcr.io/fluentci-io/clojure:latest")
-      .withMountedCache("/root/.m2", client.cacheVolume("maven"))
-      .withMountedCache("/app/target", client.cacheVolume("clojure-build"))
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec([
-        "sh",
-        "-c",
-        'eval "$(devbox global shellenv)" && lein uberjar',
-      ])
-      .withExec(["cp", "-r", "target", "/"]);
+  const context = await getDirectory(dag, src);
+  const ctr = dag
+    .pipeline(Job.uberjar)
+    .container()
+    .from("ghcr.io/fluentci-io/clojure:latest")
+    .withMountedCache("/root/.m2", dag.cacheVolume("maven"))
+    .withMountedCache("/app/target", dag.cacheVolume("clojure-build"))
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && lein uberjar'])
+    .withExec(["cp", "-r", "target", "/"]);
 
-    await ctr.stdout();
-    id = await ctr.directory("/target").id();
-  });
+  await ctr.stdout();
+  const id = await ctr.directory("/target").id();
+  await ctr.directory("/target").export("./target");
+
   return id;
 }
 
@@ -51,21 +45,18 @@ export async function uberjar(
 export async function test(
   src: Directory | string | undefined = "."
 ): Promise<string> {
-  let result = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.test)
-      .container()
-      .from("ghcr.io/fluentci-io/clojure:latest")
-      .withMountedCache("/root/.m2", client.cacheVolume("maven"))
-      .withMountedCache("/app/target", client.cacheVolume("clojure-build"))
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && lein test']);
+  const context = await getDirectory(dag, src);
+  const ctr = dag
+    .pipeline(Job.test)
+    .container()
+    .from("ghcr.io/fluentci-io/clojure:latest")
+    .withMountedCache("/root/.m2", dag.cacheVolume("maven"))
+    .withMountedCache("/app/target", dag.cacheVolume("clojure-build"))
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && lein test']);
 
-    result = await ctr.stdout();
-  });
+  const result = await ctr.stdout();
   return result;
 }
 
